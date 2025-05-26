b3a2bee41123b2ddb79fa7a08dfaacc111aa5bcb
ValentinoCattaneoLuna <valencattaneo04@gmail.com>
2025-05-26 03:16:00 -0300

    feat: uso de loadTestimonios.js

--- landingPage/js/index.js
+++ landingPage/js/index.js
@@ -1,4 +1,6 @@
 import {toggleMenu} from './components/toggleMenu.js'
+import { cargarTestimonios } from './components/loadTestimonios.js';
 document.addEventListener("DOMContentLoaded", async() => {
     toggleMenu();
+    cargarTestimonios();
   });
