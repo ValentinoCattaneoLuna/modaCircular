495352ee97957ccdb38cf42ebf0633f4574dac27
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-06-10 00:54:39 -0300

    feat: :sparkles: ruta para  endpoint de actualizarUsuarioId

--- webapp/backend/src/routes/user.routes.ts
+++ webapp/backend/src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { verUsuarios, verUsuarioPorId } from "../controllers/user.controller";
+import { verUsuarios, verUsuarioPorId, actualizarUsuarioId} from "../controllers/user.controller";
+import { authenticate } from "../middleware/auth";
 
 
 const router = Router();
@@ -8,5 +9,7 @@ const router = Router();
 router.get('/', verUsuarios)
 
 router.get('/:id_usuario',verUsuarioPorId)
+
+router.patch('/:id_usuario',authenticate, actualizarUsuarioId)
 
 export default router;
