cfceece2446ba349ffa23e6a96f92e5b04918369
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-06-10 15:39:37 -0300

    feat: pagina con loader para evitar 404 si estas logueado te lleva al feed, si no al login

---
+++ webapp/frontend/src/app/user/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useEffect } from 'react'
+import Cookies from 'js-cookie'
+import { useRouter } from 'next/navigation'
+import { RiseLoader } from "react-spinners";
+
+export default function user() {
+    const router = useRouter()
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (token) {
+                router.push('/feed')
+            } else {
+                router.push('/login')
+            }
+        }, 2000)
+        const token = Cookies.get('token')
+        return () => clearTimeout(timer);
+    }, [router])
+
+    return (
+        <div className='flex items-center justify-center flex-col gap-4 w-dvw h-dvh bg-gradient-to-b from-green-50 to-green-100'>
+
+            <div className="sweet-loading">
+                <RiseLoader color='#22c55e' />
+
+            </div>
+
+        </div>
+    )
+}
