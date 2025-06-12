9f5b5cd28d887a4d0f2ed1181c8084b2afcb25dc
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-06-12 15:14:55 -0300

    refactor: :recycle: utilizacion del componente de LoaderValidation

--- webapp/frontend/src/app/user/page.tsx
+++ webapp/frontend/src/app/user/page.tsx
@@ -1,33 +1,9 @@
 'use client'
 
-import { useEffect } from 'react'
-import Cookies from 'js-cookie'
-import { useRouter } from 'next/navigation'
-import { RiseLoader } from "react-spinners";
+import {LoaderValidation} from '@/components/loader-validation';
+
 
 export default function user() {
-    const router = useRouter()
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            if (token) {
-                router.push('/feed')
-            } else {
-                router.push('/login')
-            }
-        }, 2000)
-        const token = Cookies.get('token')
-        return () => clearTimeout(timer);
-    }, [router])
-
-    return (
-        <div className='flex items-center justify-center flex-col gap-4 w-dvw h-dvh bg-gradient-to-b from-green-50 to-green-100'>
-
-            <div className="sweet-loading">
-                <RiseLoader color='#22c55e' />
-
-            </div>
-
-        </div>
-    )
+  return <LoaderValidation validacion={true} rutaAuth='/feed' rutaSinAuth='/login'/>
+  
 }
