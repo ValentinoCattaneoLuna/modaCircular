b8b3a38c849e8cba603338f7433c9d44e68508aa
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-06-13 03:38:06 -0300

    style: 💄 añade margen inferior al layout del body

--- webapp/frontend/src/app/layout.tsx
+++ webapp/frontend/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased mb-[60px] md:mb-0`}
       >
         {children}
       </body>
