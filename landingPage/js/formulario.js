445bf33318ef7306e11145044a59688692bdfd70
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-06-18 16:11:30 -0300

    feat: :sparkles: Agregar archivo formulario.js con lógica de envío de testimonios y menú

---
+++ landingPage/js/formulario.js
@@ -0,0 +1,6 @@
+import { sendTestimonio } from "./components/sendTestimonios.js";
+import { toggleMenu } from "./components/toggleMenu.js";
+addEventListener("DOMContentLoaded", function () {
+    toggleMenu();
+    sendTestimonio();
+});
