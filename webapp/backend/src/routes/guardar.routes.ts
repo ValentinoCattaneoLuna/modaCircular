112ec80b5ecb569869f7d9dd9d8b94bda0955309
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-07-03 01:33:12 -0300

    feat: agregar autenticación a las rutas de guardar y borrar publicaciones

--- webapp/backend/src/routes/guardar.routes.ts
+++ webapp/backend/src/routes/guardar.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
-import { borrarPublicacion, guardarPublicacion } from '../controllers/guardar.controller';
-
+import { borrarPublicacion, guardarPublicacion, esFavorito,obtenerPublicacionesGuardadas } from '../controllers/guardar.controller';
+import { authenticate } from "../middleware/auth";
 const router = Router();
 
 // Ruta para registrar usuario
-router.post('/guardarPublicacion', guardarPublicacion);
+router.post('/guardarPublicacion',authenticate, guardarPublicacion);
 
 // Ruta para iniciar sesión
-router.delete('/borrarPublicacion/:id_publicacion', borrarPublicacion);
+router.delete('/borrarPublicacion/:id_publicacion', authenticate,borrarPublicacion);
 
+router.get('/estado/:id_publicacion', authenticate, esFavorito);
 
+router.get('/guardadas',authenticate,obtenerPublicacionesGuardadas)
 
 export default router;
