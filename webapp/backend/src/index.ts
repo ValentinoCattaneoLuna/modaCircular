796a308de8d72e30a708dbebd77328f0690618d7
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-07-03 06:27:23 -0300

    feat: agregar manejo de rutas de archivo en index.ts

--- webapp/backend/src/index.ts
+++ webapp/backend/src/index.ts
@@ -13,6 +13,10 @@ import type { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //conexion a la db
 connectDB();
 
