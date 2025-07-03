752902e17dd27ec2a0081daea1df331a61a13146
Valentino Cattaneo <valencattaneo04@gmail.com>
2025-07-03 18:29:01 -0300

    feat: :sparkles: sweetalert para caso de exito y de error
    reseteo del formulario cuando es enviado

--- landingPage/js/components/sendTestimonios.js
+++ landingPage/js/components/sendTestimonios.js
@@ -3,31 +3,37 @@ const formulario = document.getElementById("testimonioForm");
 
 
 export async function sendTestimonio() {
-    
-addEventListener("submit", async (event) => {
-    event.preventDefault();
-    const formData = new FormData(formulario);
-    const data = Object.fromEntries(formData.entries());    
-    try {
-        const response = await fetch("http://localhost:4000/api/testimonios/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        });
 
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
+    addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const formData = new FormData(formulario);
+        const data = Object.fromEntries(formData.entries());
+        try {
+            const response = await fetch("http://localhost:4000/api/testimonios/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            Swal.fire({
+                title: "Gracias",
+                text: "Por tu colaboracion!",
+                icon: "success"
+            });
+            formulario.reset()
+        } catch (error) {
+            Swal.fire({
+                icon: "error",
+                title: "Lo sentimos",
+                text: "Ocurrio un error inesperado!",
+            });
         }
-
-        const result = await response.json();
-        console.log("Success:", result);
-        //alert("Testimonio enviado con éxito"); //implementar un sweetalert de éxito
-    } catch (error) {
-        console.error("Error:", error);
-        //alert("Error al enviar el testimonio"); //implementar un sweetalert de error
-    }
-});
+    });
 
 }
